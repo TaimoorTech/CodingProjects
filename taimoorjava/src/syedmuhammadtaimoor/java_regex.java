aefd4efc5c1bcf5c38dfb9e4f96bb1ece129146f
SYED MUHAMMAD TAIMOOR <smtaimoor13@gmail.com>
2021-10-13 20:42:12 +0500

    "committing..."

--- taimoorjava/src/syedmuhammadtaimoor/java_regex.java
+++ taimoorjava/src/syedmuhammadtaimoor/java_regex.java
@@ -1,8 +1,24 @@
 package syedmuhammadtaimoor;
 
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class java_regex {
     public static void main(String[] args)
     {
-
+        System.out.println("Enter Email : ");
+        Scanner sc = new Scanner(System.in);
+        String email = sc.next();
+        //Pattern p = Pattern.compile("^[A-z]+[a-z]+[0-9]+@gmail\\.com$");
+        Pattern p = Pattern.compile("^[3][A-Za-z]+@gmail\\.com$");
+        Matcher m = p.matcher(email);
+        if (m.find())
+        {
+            System.out.println("Email is Valid");
+        }
+        else
+        {
+            System.out.println("Email is not Valid");
+        }
     }
 }
