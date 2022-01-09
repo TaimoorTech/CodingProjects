a5b7f9d5a32ddd54d998e54877d291ea4cca27dd
SYED MUHAMMAD TAIMOOR <smtaimoor13@gmail.com>
2022-01-09 21:06:14 +0500

    "commits"

--- DSA labs/lab 5 (a).py
+++ DSA labs/lab 5 (a).py
@@ -1,26 +1,26 @@
-def STORE_TRIANGULAR(A):
-    i = 0
-    U = []
-    size_of_lst = int(0.5 * len(A) * (len(A) + 1))
+# def STORE_TRIANGULAR(A):
+#     i = 0
+#     U = []
+#     size_of_lst = int(0.5 * len(A) * (len(A) + 1))
+#
+#     for size in range(size_of_lst):
+#         U.append(0)
+#
+#     for j in range(len(A)):
+#         for k in range(j + 1):
+#             U[i] = A[j][k]
+#             i += 1
+#
+#     return U
+#
+#
+# triangular_matrix = [[-4, 0, 0, 0], [65, -88, 0, 0], [-24, -41, 59, 0], [-7, -32, -14, -99]]
+# print("Triangular Matrix  : ", triangular_matrix)
+#
+# unidimensional_array = STORE_TRIANGULAR(triangular_matrix)
+# print("After Storing Triangular Matrix into Unidimensional : ", unidimensional_array)
 
-    for size in range(size_of_lst):
-        U.append(0)
-
-    for j in range(len(A)):
-        for k in range(j + 1):
-            U[i] = A[j][k]
-            i += 1
-
-    return U
-
-
-triangular_matrix = [[-4, 0, 0, 0], [65, -88, 0, 0], [-24, -41, 59, 0], [-7, -32, -14, -99]]
-print("Triangular Matrix  : ", triangular_matrix)
-
-unidimensional_array = STORE_TRIANGULAR(triangular_matrix)
-print("After Storing Triangular Matrix into Unidimensional : ", unidimensional_array)
-
-length = len(triangular_matrix)
+# length = len(triangular_matrix)
 
 
 def RETRIEVE_TRIANGULAR(U, n):
@@ -42,8 +42,9 @@ def RETRIEVE_TRIANGULAR(U, n):
     return A
 
 
+unidimensional_array = [-4, 65, -88, -24, -41, 59, -7, -32, -14, -99]
 print("Unidimensional array : ", unidimensional_array)
 
-converted_triangular_matrix = RETRIEVE_TRIANGULAR(unidimensional_array, length)
+converted_triangular_matrix = RETRIEVE_TRIANGULAR(unidimensional_array, 4)
 print("Retrieving Triangular Matrix from Unidimensional array: ", converted_triangular_matrix)
 
