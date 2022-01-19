cfd9be81ff0f950c3487aa81d1d0a244500c8524
SYED MUHAMMAD TAIMOOR <smtaimoor13@gmail.com>
2022-01-20 02:17:20 +0500

    commit

---
+++ AndroidStudioProjects/CEW_CEP_CS031_CS010_CS006/app/src/main/java/com/example/cew_cep_cs031_cs010_cs006/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.cew_cep_cs031_cs010_cs006;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button signup_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signup_button = findViewById(R.id.signup);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, signup.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
