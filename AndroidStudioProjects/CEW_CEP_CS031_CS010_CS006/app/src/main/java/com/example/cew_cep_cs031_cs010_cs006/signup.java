a7a49a9074ba3e674441ddb73cc183ea21b31dd1
SYED MUHAMMAD TAIMOOR <smtaimoor13@gmail.com>
2022-01-21 01:32:23 +0500

    "commit"

--- AndroidStudioProjects/CEW_CEP_CS031_CS010_CS006/app/src/main/java/com/example/cew_cep_cs031_cs010_cs006/signup.java
+++ AndroidStudioProjects/CEW_CEP_CS031_CS010_CS006/app/src/main/java/com/example/cew_cep_cs031_cs010_cs006/signup.java
@@ -3,12 +3,22 @@ package com.example.cew_cep_cs031_cs010_cs006;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class signup extends AppCompatActivity {
-
+    private Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(signup.this, "You have been Registered....", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
