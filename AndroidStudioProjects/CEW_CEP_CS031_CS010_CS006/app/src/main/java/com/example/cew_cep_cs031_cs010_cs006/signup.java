3aee325c6392bf271e1a6226e3da88e11cd37b9c
SYED MUHAMMAD TAIMOOR <smtaimoor13@gmail.com>
2022-01-23 20:53:17 +0500

    "commit"

--- AndroidStudioProjects/CEW_CEP_CS031_CS010_CS006/app/src/main/java/com/example/cew_cep_cs031_cs010_cs006/signup.java
+++ AndroidStudioProjects/CEW_CEP_CS031_CS010_CS006/app/src/main/java/com/example/cew_cep_cs031_cs010_cs006/signup.java
@@ -2,6 +2,7 @@ package com.example.cew_cep_cs031_cs010_cs006;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -22,27 +23,49 @@ public class signup extends AppCompatActivity {
         username = findViewById(R.id.username);
         email_signup = findViewById(R.id.email);
         password_signup = findViewById(R.id.password);
-        database creating = new database(signup.this);
+        database signing = new database(signup.this);
 
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int i = 0;
                 String user1 = username.getText().toString();
                 String email1 = email_signup.getText().toString();
                 String pass1 = password_signup.getText().toString();
-                if(user1.equals("") || email1.equals("") || pass1.equals("")){
-                    Toast.makeText(signup.this, "Fill the fields to Sign Up........", Toast.LENGTH_LONG).show();
+                if(email1.endsWith("@gmail.com" )|| email1.endsWith("@yahoo.com")){
+                    if(user1.equals("") || email1.equals("") || pass1.equals("")){
+                        Toast.makeText(signup.this, "Fill the fields to Sign Up........", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        Cursor checking_row = signing.get_info();
+                        while(checking_row.moveToNext()){
+                            if(checking_row.getString(2).equals(email1)){
+                                Toast.makeText(signup.this, "Account with this Email has already Signed Up ....", Toast.LENGTH_LONG).show();
+                                i=1;
+                                break;
+                            }
+                            else if(checking_row.getString(1).equals(user1)){
+                                Toast.makeText(signup.this, "Account with this Username has already Signed Up ....", Toast.LENGTH_LONG).show();
+                                i=1;
+                                break;
+                            }
+                        }
+                        if (i==0){
+                            Boolean result = signing.data_insertion_database(user1, email1, pass1);
+                            if (result == true){
+                                Toast.makeText(signup.this, "Account is Successfully Registered....", Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                Toast.makeText(signup.this, "Account is not Successfully Registered....", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
                 }
                 else{
-                    Boolean result = creating.data_insertion_database(user1, email1, pass1);
-                    if (result == true){
-                        Toast.makeText(signup.this, "Account is Successfully Registered....", Toast.LENGTH_LONG).show();
-                    }
-                    else {
-                        Toast.makeText(signup.this, "Account is not Successfully Registered....", Toast.LENGTH_LONG).show();
-                    }
+                  Toast.makeText(signup.this, "Incorrect Email Format!!", Toast.LENGTH_LONG).show();
                 }
+
             }
         });
     }
