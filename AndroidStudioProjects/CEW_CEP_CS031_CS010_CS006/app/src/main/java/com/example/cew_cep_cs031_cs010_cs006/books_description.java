e3a7c1fd5415b8180657d348be9cc01011a491a3
SYED MUHAMMAD TAIMOOR <smtaimoor13@gmail.com>
2022-01-30 03:50:40 +0500

    "latest commit"

--- AndroidStudioProjects/CEW_CEP_CS031_CS010_CS006/app/src/main/java/com/example/cew_cep_cs031_cs010_cs006/books_description.java
+++ AndroidStudioProjects/CEW_CEP_CS031_CS010_CS006/app/src/main/java/com/example/cew_cep_cs031_cs010_cs006/books_description.java
@@ -9,26 +9,54 @@ public class books_description {
     }
 
     public  String[] get_description(){
-        ArrayList<String> descriptions = new ArrayList<>();
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2010.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2011.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2012.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2013.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2014.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2015.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2016.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2017.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2018.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2019.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2020.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2021.");
-        descriptions.add("The Author of the Book is JK Rowling. It was released in 2022.");
-        String[] des = {"The Author of the Book is JK Rowling. It was released in 2010.",
-                        "The Author of the Book is JK Rowling. It was released in 2011.",
-                        "The Author of the Book is JK Rowling. It was released in 2012.",
-                        "The Author of the Book is JK Rowling. It was released in 2013.",
-                        "The Author of the Book is JK Rowling. It was released in 2014.",
-                        "The Author of the Book is JK Rowling. It was released in 2015.",
+        String[] des = {"Harry Potter and the Philosopher's Stone is a fantasy novel." +
+                "The first novel in the Harry Potter series and Rowling's debut novel," +
+                "it follows Harry Potter, a young wizard who discovers his magical heritage " +
+                "on his eleventh birthday,when he receives a letter of acceptance to Hogwarts " +
+                "School of Witchcraft and Wizardry. and the Journey Begins.\n\n" +
+                "The book was first published in the United Kingdom on 26 June 1997 by Bloomsbury." +
+                "The novel has sold in excess of 120 million copies, " +
+                "making it the third best-selling novel of all time. ",
+
+                "Harry Potter and the Chamber of Secrets is a fantasy novel written by British author " +
+                        "J.K. Rowling and the second novel in the Harry Potter series. " +
+                        "The plot follows Harry's second year at Hogwarts School of Witchcraft and Wizardry, " +
+                        "during which a series of messages on the walls of the school's corridors " +
+                        "warn that the \"Chamber of Secrets\" has been opened and that the " +
+                        "\"heir of Slytherin\" would kill all pupils w" +
+                        "ho do not come from all-magical families.\n\nThe book was published in the " +
+                        "United Kingdom on 2 July 1998 by Bloomsbury and later in the United States " +
+                        "on 2 June 1999 by Scholastic Inc.",
+
+                "Harry Potter and the Prisoner of Azkaban is a fantasy novel written by British "+
+                        "author J.K. Rowling and is the third in the Harry Potter series. " +
+                        "The book follows Harry Potter, a young wizard, in his third year " +
+                        "at Hogwarts School of Witchcraft and Wizardry. Along with friends " +
+                        "Ronald Weasley and Hermione Granger, Harry investigates Sirius Black, " +
+                        "an escaped prisoner from Azkaban, the wizard prison, believed to be " +
+                        "one of Lord Voldemort's old allies.\n\nThe book was published in the United" +
+                        " Kingdom on 8 July 1999 by Bloomsbury and in the United States on 8 " +
+                        "September 1999 by Scholastic, Inc.The film adaptation of the novel was " +
+                        "released in 2004, grossing more than $796 million and earning critical " +
+                        "acclaim.",
+
+                "Amara the Brave is a novel written by Matt Zhang.",
+
+                "The Author of the Book is JK Rowling. It was released in 2014.",
+
+
+                "Catching Fire is a 2009 science fiction young adult novel by the American novelist " +
+                        "Suzanne Collins, the second book in The Hunger Games series. As the sequel " +
+                        "to the 2008 bestseller The Hunger Games, it continues the story of Katniss " +
+                        "Everdeen and the post-apocalyptic nation of Panem. Following the events of " +
+                        "the previous novel, a rebellion against the oppressive Capitol has begun, " +
+                        "and Katniss and fellow tribute Peeta Mellark are forced to return to the " +
+                        "arena in a special edition of the Hunger Games.\n\nThe book was first " +
+                        "published on September 1, 2009, by Scholastic, in hardcover, and was later " +
+                        "released in ebook and audiobook format. Catching Fire received mostly " +
+                        "positive reviews. The book has sold more than 19 million copies in the U.S." +
+                        " alone.",
+
                         "The Author of the Book is JK Rowling. It was released in 2016.",
                         "The Author of the Book is JK Rowling. It was released in 2017.",
                         "The Author of the Book is JK Rowling. It was released in 2018.",
